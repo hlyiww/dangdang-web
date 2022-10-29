cbfd581b8250171976c426c283f0f420005c231c
yiwwhl <yiwwhl@qq.com>
2022-10-29 15:16:58 +0800

    chore: project server config

---
+++ src/declare/serverConfig.d.ts
@@ -0,0 +1,10 @@
+import "dotenv";
+
+declare module "dotenv" {
+  export interface DotenvParseOutput {
+    VITE_HOST: string;
+    VITE_PORT: number;
+    VITE_BACKEND_PREFIX: string;
+    VITE_PROXY_DOMAIN: string;
+  }
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,35 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig, ServerOptions } from "vite";
+import vue from "@vitejs/plugin-vue";
+import dotenv from "dotenv";
+import { readFileSync } from "fs";
+
+let server: ServerOptions;
+
+const getServerConfig = (mode: string): ServerOptions => {
+  const basicEnvFileName = ".env";
+  const currentModeEnvFileName = `${basicEnvFileName}.${mode}`;
+  const {
+    VITE_HOST: host,
+    VITE_PORT: port,
+    VITE_BACKEND_PREFIX: backendPrefix,
+    VITE_PROXY_DOMAIN: target,
+  } = dotenv.parse(readFileSync(currentModeEnvFileName));
+  return {
+    host,
+    port,
+    proxy: {
+      [backendPrefix]: {
+        target,
+      },
+    },
+  };
+};
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()]
-})
+export default defineConfig(({ mode }) => {
+  server = getServerConfig(mode);
+  return {
+    plugins: [vue()],
+    server,
+  };
+});
